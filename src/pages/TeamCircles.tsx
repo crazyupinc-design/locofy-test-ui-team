f5b9ec946bacc2ad340b4da1ca7c847d4ce89f13
디자이너 <crazyupdesigner@dijaineoui-iMac.local>
2023-05-18 10:15:56 +0900

    https://www.figma.com/file/Y2GVmBMCVG1KJjWvVlkqqs/locofy-study?type=design&node-id=2-245

--- src/pages/TeamCircles.tsx
+++ src/pages/TeamCircles.tsx
@@ -6,22 +6,26 @@ const TeamCircles: FunctionComponent = () => {
       <div className={styles.container}>
         <div className={styles.titleSection}>
           <b className={styles.teamSection}>Team section</b>
-          <div className={styles.loremIpsumDolor}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            tincidunt sagittis eros. Quisque quis euismod lorem.
+          <div className={styles.loremIpsumDolorContainer}>
+            <p
+              className={styles.loremIpsumDolor}
+            >{`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse `}</p>
+            <p className={styles.loremIpsumDolor}>
+              tincidunt sagittis eros. Quisque quis euismod lorem.
+            </p>
           </div>
         </div>
-        <div className={styles.columns}>
+        <article className={styles.columns}>
           <div className={styles.card}>
             <div className={styles.imageWrapper}>
               <img className={styles.imageIcon} alt="" src="/image@2x.png" />
             </div>
-            <div className={styles.content}>
+            <div className={styles.titleSection}>
               <div className={styles.info}>
                 <b className={styles.fullName}>Full name</b>
                 <div className={styles.jobTitle}>Job title</div>
               </div>
-              <div className={styles.loremIpsumDolor}>
+              <div className={styles.loremIpsumDolorContainer}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Suspendisse varius enim in eros elementum tristique.
               </div>
@@ -29,14 +33,14 @@ const TeamCircles: FunctionComponent = () => {
           </div>
           <div className={styles.card}>
             <div className={styles.imageWrapper}>
-              <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+              <img className={styles.imageIcon1} alt="" src="/image@2x.png" />
             </div>
-            <div className={styles.content}>
+            <div className={styles.titleSection}>
               <div className={styles.info}>
                 <b className={styles.fullName}>Full name</b>
                 <div className={styles.jobTitle}>Job title</div>
               </div>
-              <div className={styles.loremIpsumDolor}>
+              <div className={styles.loremIpsumDolorContainer}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Suspendisse varius enim in eros elementum tristique.
               </div>
@@ -44,20 +48,20 @@ const TeamCircles: FunctionComponent = () => {
           </div>
           <div className={styles.card}>
             <div className={styles.imageWrapper}>
-              <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+              <img className={styles.imageIcon1} alt="" src="/image@2x.png" />
             </div>
-            <div className={styles.content}>
+            <div className={styles.titleSection}>
               <div className={styles.info}>
                 <b className={styles.fullName}>Full name</b>
                 <div className={styles.jobTitle}>Job title</div>
               </div>
-              <div className={styles.loremIpsumDolor}>
+              <div className={styles.loremIpsumDolorContainer}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Suspendisse varius enim in eros elementum tristique.
               </div>
             </div>
           </div>
-        </div>
+        </article>
       </div>
     </div>
   );
